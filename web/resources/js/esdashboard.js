4a62fe199732a6a8ce8c90fd885f21d76a05e29e
Sergey <Sergey@10.1.10.132>
2016-06-17 15:44:56 -0700

    show details on measures is done

--- web/resources/js/esdashboard.js
+++ web/resources/js/esdashboard.js
@@ -64,3 +64,33 @@ function Warn() {
     alert("This is a warning message!");
     document.write("This is a warning message!");
 }
+
+//-------------------- dialog box ---------------------------------
+
+function CustomAlert(){
+    this.render = function(dialog){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+//        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+    };
+	this.ok = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	};
+}
+var Alert = new CustomAlert();
+
+/* ------------------- graph down -------------------------------------*/
+
+function graphShow () {
+    document.getElementById("graphShow").style.height = "500px";
+}
